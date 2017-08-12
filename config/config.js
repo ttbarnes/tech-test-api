b258436c6adee8f294df97445d889fbb81a9d34a
ttbarnes <tony@tonybarnes.me>
2017-08-12 11:48:14 +0100

    remove auth

--- config/config.js
+++ config/config.js
@@ -9,19 +9,7 @@ const envVarsSchema = Joi.object({
     .allow(['development', 'production', 'test', 'provision'])
     .default('development'),
   PORT: Joi.number()
-    .default(4040),
-  MONGOOSE_DEBUG: Joi.boolean()
-    .when('NODE_ENV', {
-      is: Joi.string().equal('development'),
-      then: Joi.boolean().default(true),
-      otherwise: Joi.boolean().default(false)
-    }),
-  JWT_SECRET: Joi.string().required()
-    .description('JWT Secret required to sign'),
-  MONGO_HOST: Joi.string().required()
-    .description('Mongo DB host url'),
-  MONGO_PORT: Joi.number()
-    .default(27017)
+    .default(4040)
 }).unknown()
   .required();
 
@@ -33,8 +21,6 @@ if (error) {
 const config = {
   env: envVars.NODE_ENV,
   port: envVars.PORT,
-  mongooseDebug: envVars.MONGOOSE_DEBUG,
-  jwtSecret: envVars.JWT_SECRET,
   mongo: {
     host: envVars.MONGO_HOST,
     port: envVars.MONGO_PORT
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import userRoutes from './user.route';
-import authRoutes from './auth.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -11,8 +10,5 @@ router.get('/health-check', (req, res) =>
 
 // mount user routes at /users
 router.use('/users', userRoutes);
-
-// mount auth routes at /auth
-router.use('/auth', authRoutes);
 
 export default router;
