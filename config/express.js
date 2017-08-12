52cffc1b78de9c25a110e01751747cae264aa581
ttbarnes <tony@tonybarnes.me>
2017-08-12 12:41:06 +0100

    cors

--- config/express.js
+++ config/express.js
@@ -32,7 +32,9 @@ app.use(methodOverride());
 app.use(helmet());
 
 // enable CORS - Cross Origin Resource Sharing
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 // enable detailed API logging in dev env
 if (config.env === 'development') {
